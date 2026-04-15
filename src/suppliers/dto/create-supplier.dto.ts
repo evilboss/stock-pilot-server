e099a16b724d4a306408846c83974498fcbd192c
evilboss <jr.evilboss@gmail.com>
2026-04-16 03:54:28 +0800

    add CreateSupplierDto to define supplier creation data structure with validation

---
+++ src/suppliers/dto/create-supplier.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsOptional, IsEmail } from 'class-validator';
+
+export class CreateSupplierDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  address?: string;
+}
