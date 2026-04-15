23f957e47fb0731905ab779928f51b36b11ae940
evilboss <jr.evilboss@gmail.com>
2026-04-16 03:53:22 +0800

    add AppModule to configure application modules and global settings

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { WarehousesModule } from './warehouses/warehouses.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { AuditLogsModule } from './audit-logs/audit-logs.module';
+import { RolesModule } from './roles/roles.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    RolesModule,
+    ProductsModule,
+    CategoriesModule,
+    SuppliersModule,
+    WarehousesModule,
+    InventoryModule,
+    TransactionsModule,
+    AuditLogsModule,
+  ],
+})
+export class AppModule {}
